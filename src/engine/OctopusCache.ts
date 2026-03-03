5826ba94f8bceacf332869acbd8b97a71098a4ef
Miguel Candeira <mcandeira.code@gmail.com>
2026-03-03 15:37:23 +0100

    [FIX] OctopusCache length bug

--- src/engine/OctopusCache.ts
+++ src/engine/OctopusCache.ts
@@ -67,6 +67,8 @@ export class OctopusCache {
 
     private stringToHash(string: string): number
     {
+        const length = string.length
+
         let hash = 5381
 
         for(let i = 0; i < length; i++){
